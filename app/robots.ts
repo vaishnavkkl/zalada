495ed1c981b9769b5a804ff367e95efa835fd223
vaishnavkkl <vaishnavkkl25@gmail.com>
2026-04-26 20:29:21 +0530

    fix static

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,8 @@
 import { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+export const revalidate = 0
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
